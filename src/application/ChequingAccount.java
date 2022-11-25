c35437dc2c2b3e92d0aa8c27cb58ae03f9cf3521
CS219-user <CS219-user@10.0.0.140>
2022-11-25 11:46:32 -0700

    **Changes associated with previous commit weren't staged
    No additional changes were made to the code compared to last commit

---
+++ src/application/ChequingAccount.java
@@ -0,0 +1,7 @@
+package application;
+
+public class ChequingAccount extends BankAccount {
+	ChequingAccount(String nameOfAccount) {
+		super(nameOfAccount);
+	}
+}
---
+++ src/application/SavingsAccount.java
@@ -0,0 +1,7 @@
+package application;
+
+public class SavingsAccount extends BankAccount {
+	SavingsAccount(String nameOfAccount) {
+		super(nameOfAccount);
+	}
+}
